c03e6df8c11d20e870069c2018eb16e8d740e6c7
VasyaTarn <vasatopktoneverittomuvlob@gmail.com>
2023-07-10 23:24:46 +0300

    Add project files.

---
+++ Wpf_12/ColorVM.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Wpf_12
+{
+    internal class ColorVM : INotifyPropertyChanged
+    {
+        private ColorModel model;
+        private Color color;
+        public ColorVM()
+        {
+            model = new ColorModel { Alpha = 50, Red = 50, Green = 50, Blue = 50 };
+        }
+
+        public int Alpha
+        {
+            get { return model.Alpha; }
+            set
+            {
+                model.Alpha = value;
+                OnPropertyChanged("Alpha");
+            }
+        }
+
+        public int Red
+        {
+            get { return model.Red; }
+            set
+            {
+                model.Red = value;
+                OnPropertyChanged("Red");
+            }
+        }
+
+        public int Green
+        {
+            get { return model.Green; }
+            set
+            {
+                model.Green = value;
+                OnPropertyChanged("Green");
+            }
+        }
+
+        public int Blue
+        {
+            get { return model.Blue; }
+            set
+            {
+                model.Blue = value;
+                OnPropertyChanged("Blue");
+            }
+        }
+
+        public Color Color
+        {
+            set
+            {
+                color = Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue);
+                OnPropertyChanged("Color");
+            }
+            get { return Color.FromArgb((byte)Alpha, (byte)Red, (byte)Green, (byte)Blue); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Wpf_12/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_12
+{
+    
+    public partial class MainWindow : Window
+    {
+        private ColorVM viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new ColorVM();
+            this.DataContext = viewModel;
+            UpdateColor();
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            viewModel = (ColorVM)this.DataContext;
+            if (viewModel != null)
+            {
+                viewModel.Alpha = (int)e.NewValue;
+                UpdateColor();
+            }
+        }
+        private void Slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            viewModel = (ColorVM)this.DataContext;
+            if (viewModel != null)
+            {
+                viewModel.Red = (int)e.NewValue;
+                UpdateColor();
+            }
+        }
+        private void Slider3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            viewModel = (ColorVM)this.DataContext;
+            if (viewModel != null)
+            {
+                viewModel.Green = (int)e.NewValue;
+                UpdateColor();
+            }
+        }
+        private void Slider4_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            viewModel = (ColorVM)this.DataContext;
+            if (viewModel != null)
+            {
+                viewModel.Blue = (int)e.NewValue;
+                UpdateColor();
+            }
+        }
+
+        private void UpdateColor()
+        {
+            if (viewModel != null)
+            {
+                viewModel.Color = Color.FromArgb((byte)viewModel.Alpha, (byte)viewModel.Red, (byte)viewModel.Green, (byte)viewModel.Blue);
+            }
+        }
+    }
+}
